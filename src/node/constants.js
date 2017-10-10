5336fa8588d4c07c27b1d37675006ebf235d30fd
Sudhakar <sudhakar.sw@gmail.com>
2017-10-10 07:08:27 +0530

    Update constants.js

--- src/node/constants.js
+++ src/node/constants.js
@@ -5,7 +5,7 @@ module.exports = Object.freeze({
   INLINE_CSS_MARKER: '<!-- INLINE CSS HERE, BATFISH -->',
   BATFISH_CSS_BASENAME: 'batfish-styles.css',
   STATS_BASENAME: 'stats.json',
-  PUBLIC_PATH_ASSETS: 'assets',
+  PUBLIC_PATH_ASSETS: 'site_assets',
   DATA_DIRECTORY: 'data',
   EVENT_ERROR: 'error',
   EVENT_NOTIFICATION: 'notification',
